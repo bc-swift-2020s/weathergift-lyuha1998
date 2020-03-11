cf982f70dc62de4c3652d3d4bd51a8f28a900275
Huiyi Victoria Lyu <lyuha@bc.edu>
2020-03-11 14:32:02 -0400

    with gating.nore

---
+++ weathergift/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  weathergift
+//
+//  Created by Huiyi Victoria Lyu on 3/11/20.
+//  Copyright © 2020 Huiyi Victoria Lyu. All rights reserved.
+//
+
+import Foundation
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyBwJ79fD5gHPJJGjmOkT0wrZ9X0U3c8c8o"
+}
---
+++ weathergift/weatherlocation.swift
@@ -0,0 +1,15 @@
+//
+//  weatherlocation.swift
+//  weathergift
+//
+//  Created by Huiyi Victoria Lyu on 3/9/20.
+//  Copyright © 2020 Huiyi Victoria Lyu. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
