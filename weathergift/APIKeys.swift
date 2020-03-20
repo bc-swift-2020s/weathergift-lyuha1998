b7660177c0e08fb5e79b4a4f4c4e5a0e733126f4
Huiyi Victoria Lyu <lyuha@bc.edu>
2020-03-20 13:47:28 -0400

    done!!!

--- weathergift/APIKeys.swift
+++ weathergift/APIKeys.swift
@@ -9,4 +9,10 @@
 import Foundation
 struct APIkeys {
     static let googlePlacesKey = "AIzaSyBwJ79fD5gHPJJGjmOkT0wrZ9X0U3c8c8o"
+    static let darkSkyKey = "c555ece76849734d296fe7dee0552676"
+}
+
+
+struct APIurls {
+    static let darkSkyURL = "https://api.darksky.net/forecast/"
 }
